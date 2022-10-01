92b950e3bc5c77c30402844c632e2cee5c3dbeeb
Ron Georgia <rong1611@gmail.com>
2022-10-01 15:09:18 -0400

    WIP

--- src/fvwm_menu_desktop_args.py
+++ src/fvwm_menu_desktop_args.py
@@ -8,6 +8,7 @@ from fvwm_menu_desktop_constants import (
     DEFAULT_ICON_DIR,
     DEFAULT_APP_NAME,
     DEFAULT_ICON_SIZE,
+    DEFAULT_GNOME_FS_DIR,
 )
 
 
@@ -40,19 +41,19 @@ def desktop_menu_args():
         "-w",
         "--with-titles",
         dest="with_titles",
+        action=argparse.BooleanOptionalAction,
         help="Generate menus with titles. Default.",
     )
-    # should be a true/false
     parser.add_argument(
         "--without-titles",
         dest="without_titles",
-        type=bool,
+        action=argparse.BooleanOptionalAction,
         help="generate menus without titles.",
     )
     parser.add_argument(
         "--enable-mini-icons",
         dest="enable_mini_icons",
-        type=bool,
+        action=argparse.BooleanOptionalAction,
         help="enable mini-icons in menu.",
     )
 
@@ -93,12 +94,91 @@ def desktop_menu_args():
         "--dir-icon",
         dest="dir_icon",
         metavar="<NAME>",
-        help="set default directory icon if no others found.",
+        default=DEFAULT_GNOME_FS_DIR,
+        help="set default directory icon if no others found. Default is 'gnome-fs-directory'.",
     )
-    # parser.add_argument("", dest="", metavar="", help="")
-    # parser.add_argument("", dest="", metavar="", help="")
-    # parser.add_argument("", dest="", metavar="", help="")
-    # parser.add_argument("", dest="", metavar="", help="")
-    # parser.add_argument("", dest="", metavar="", help="")
+    parser.add_argument(
+        "-t",
+        "--title",
+        dest="title",
+        metavar="<NAME>",
+        type=str,
+        help="menu title of the top menu used by Popup command. Default is XDGMenu.",
+    )
+    parser.add_argument(
+        "--insert-in-menu",
+        dest="insert_in_menu",
+        metavar="<NAME>",
+        type=str,
+        help="generates a menu to place it in the root level of the menu NAME.",
+    )
+    parser.add_argument(
+        "--get-menus",
+        dest="get_menus",
+        choices=["all", "desktop"],
+        help="Prints a space separated list of full menu paths. "
+        "'all' is all menus on the system except empty ones. "
+        "'desktop' list the menus that would have been generated. "
+        "No menu generation is done.",
+    )
+    parser.add_argument(
+        "--set-menus",
+        dest="set_menu",
+        metavar="<MENU PATHS>",
+        help="expects a space separated list of full menu paths to generate user specified menus.",
+    )
+    parser.add_argument(
+        "--all-menus",
+        dest="all_menus",
+        action=argparse.BooleanOptionalAction,
+        help="Generate all menus found",
+    )
+
+    parser.add_argument(
+        "--include-items",
+        dest="include_items",
+        choices=["config", "regenerate", "both", None],
+        default="both",
+        help="include additional menu items NAME in top level menu. "
+        "NAME can be 'config', 'regenerate', 'both' or 'none'. Default both.",
+    )
+    parser.add_argument(
+        "--regen-cmd",
+        dest="regen_cmd",
+        metavar="<ACTION>",
+        type=str,
+        default="PipeRead `fvwm-menu-desktop`",
+        help="The fvwm ACTION for the 'Regenerate' menu item. "
+        "Default: 'PipeRead `fvwm-menu-desktop`'",
+    )
+    parser.add_argument(
+        "--term-cmd",
+        dest="term_cmd",
+        metavar="<CMD>",
+        type=str,
+        default="xterm -e",
+        help="Terminal emulator CMD used on terminal entries. Default: xterm -e",
+    )
+
+    parser.add_argument(
+        "--dynamic",
+        dest="dynamic",
+        action=argparse.BooleanOptionalAction,
+        help="used with dynamic menus",
+    )
+    parser.add_argument(
+        "-e",
+        "--menu-error",
+        dest="menu_error",
+        action=argparse.BooleanOptionalAction,
+        help="out python-xdg not found error in menu.",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="verbose",
+        action=argparse.BooleanOptionalAction,
+        help="run and display debug info on STDERR",
+    )
 
     return parser.parse_args()
--- src/fvwm_menu_desktop_constants.py
+++ src/fvwm_menu_desktop_constants.py
@@ -6,3 +6,4 @@ DEFAULT_ICON_DIR = "~/.fvwm/icons"
 
 DEFAULT_ICON_SIZE = 24
 DEFAULT_APP_NAME = "gnome-applications"
+DEFAULT_GNOME_FS_DIR = "gnome-fs-directory"
